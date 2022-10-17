a1bd069de9b196ba1f93a2bea054cba4ffaac70a
León G <A00573074@itesm.mx>
2022-10-17 22:51:49 +0000

    New

--- main1.cpp
+++ main1.cpp
@@ -62,4 +62,5 @@ REFERENCIAS:
     https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/ [SI DA EL CASO DE PRUEBA DEL SITIO].
     https://www.tutorialspoint.com/cplusplus-program-to-solve-the-0-1-knapsack-problem [SI DA EL CASO DE PRUEBA DEL SITIO].
     https://www.sanfoundry.com/cpp-program-solve-knapsack-problem-using-dynamic-programming/ [SI DA EL CASO DE PRUEBA DEL SITIO].
+    https://www.tutorialspoint.com/cplusplus-program-to-solve-knapsack-problem-using-dynamic-programming [SI DA EL CASO DE PRUEBA DEL SITIO].
 */
