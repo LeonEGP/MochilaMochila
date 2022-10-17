ca90de813f5f0e71d2eed7515b49af4b8ad70b42
León G <A00573074@itesm.mx>
2022-10-17 22:32:34 +0000

    New

--- main1.cpp
+++ main1.cpp
@@ -51,7 +51,6 @@ int main() {
 
     cin >> w;
 
-    espacio();
     cout << mochila(w,valores,pesos,n);
     espacio();
 
@@ -62,5 +61,5 @@ int main() {
 REFERENCIAS:
     https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/ [SI DA EL CASO DE PRUEBA DEL SITIO].
     https://www.tutorialspoint.com/cplusplus-program-to-solve-the-0-1-knapsack-problem [SI DA EL CASO DE PRUEBA DEL SITIO].
-    https://www.sanfoundry.com/cpp-program-solve-knapsack-problem-using-dynamic-programming/ [SI DA LOS CASOS DE PRUEBA DEL SITIO].
+    https://www.sanfoundry.com/cpp-program-solve-knapsack-problem-using-dynamic-programming/ [SI DA EL CASO DE PRUEBA DEL SITIO].
 */
--- main2.cpp
+++ main2.cpp
@@ -15,17 +15,31 @@ int mayor(int x, int y) {
     }
 }
 
-int knapsack(int capacidad, int valores[], int pesos[], int n) {
-    mochila[n+1][capacidad+1];
+int knapsack (int capacidad, int valores[], int pesos[], int n) {
+    
+    int resultado;
+    int mochila[n + 1][capacidad+1];
 
-    for (int i = 0; i <= n; i++){
-        for (int j = 0; i <= n; )
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= capacidad; j++) {
+            if (i == 0 || j == 0) {
+                mochila[i][j] = 0;
+            }
+            else if (pesos[i-1] <= j) {
+                mochila[i][j] = mayor(valores[i-1] + mochila[i-1][j-pesos[i-1]],mochila[i-1][j]);
+            }
+            else {
+                mochila[i][j] = mochila[i - 1][j];
+            }
+        }
     }
 
+    resultado = mochila[n][capacidad];
+    return resultado;
 }
 
 int main() {
-    
+
     int n;
     int w;
 
@@ -42,13 +56,12 @@ int main() {
     for (int j = 0; j < n; j++) {
         int temporal;
         cin >> temporal;
-        pesos[i] = temporal;
+        pesos[j] = temporal;
     }
 
     cin >> w;
 
-    espacio();
-    cout << knapsack(w,valores,pesos,n);
+    cout << knapsack(w, valores, pesos, n);
     espacio();
 
     return 0;
@@ -58,5 +71,5 @@ int main() {
 REFERENCIAS:
     https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/ [SI DA EL CASO DE PRUEBA DEL SITIO].
     https://www.tutorialspoint.com/cplusplus-program-to-solve-the-0-1-knapsack-problem [SI DA EL CASO DE PRUEBA DEL SITIO].
-    https://www.sanfoundry.com/cpp-program-solve-knapsack-problem-using-dynamic-programming/ [SI DA LOS CASOS DE PRUEBA DEL SITIO].
+    https://www.sanfoundry.com/cpp-program-solve-knapsack-problem-using-dynamic-programming/ [SI DA EL CASO DE PRUEBA DEL SITIO].
 */
